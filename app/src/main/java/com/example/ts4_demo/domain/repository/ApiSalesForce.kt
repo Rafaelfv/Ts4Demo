6616a0903766e122c94780b9b1c8098c78a34979
Rafael Flores <jibaro.desarrollo@gmail.com>
2022-11-08 00:59:33 -0600

    se generan ambas instancias de apis, listas para ser usadas

--- app/src/main/java/com/example/ts4_demo/domain/repository/ApiSalesForce.kt
+++ app/src/main/java/com/example/ts4_demo/domain/repository/ApiSalesForce.kt
@@ -1,12 +1,13 @@
 package com.example.ts4_demo.domain.repository
 
 import io.reactivex.Single
+import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiSalesForce {
 
     @GET("/services/data/v46.0/sobjects/Contact/")
-    fun getContacts( ): Single<Response<okhttp3.Response>>
+    fun getContacts( ): Single<Response<ResponseBody>>
 
 }
--- app/src/main/java/com/example/ts4_demo/domain/viewModels/SignInViewModel.kt
+++ app/src/main/java/com/example/ts4_demo/domain/viewModels/SignInViewModel.kt
@@ -16,9 +16,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import retrofit2.Response
-import retrofit2.Retrofit
 import javax.inject.Inject
-import javax.inject.Named
 
 class SignInViewModel : BaseViewModel() {
 
@@ -27,28 +25,10 @@ class SignInViewModel : BaseViewModel() {
     @Inject
     lateinit var apiSF:ApiSalesForce
 
-
     var loadingVisibility: MutableLiveData<Int> = MutableLiveData()
     var isLogin: MutableLiveData<Boolean> = MutableLiveData()
     private lateinit var subscription: Disposable
 
-    fun signIn(user: EditText?, pass: EditText?){
-        val a = 0
-
-        subscription = apiSF.getContacts()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .doOnSubscribe { loadingVisibility.value = View.VISIBLE }
-            .doOnTerminate { loadingVisibility.value = View.GONE }
-            .subscribe(
-                { it ->
-                    val a = 0
-                }, { error ->
-                    val a = 0
-                }
-            )
-    }
-/*
     fun signIn(user: EditText?, pass: EditText?) {
 
         if (user?.text!!.isEmpty()) {
@@ -76,8 +56,6 @@ class SignInViewModel : BaseViewModel() {
                 }
             )
     }
-
- */
 
     private fun onSuccessSigUp(response: Response<LoginResponse>?) {
 
